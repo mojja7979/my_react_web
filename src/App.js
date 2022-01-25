afa7f0b06d0b185a2240674a88d9552932a424bb
mojja7979 <pcs386x@naver.com>
2022-01-25 23:59:20 +0900

    [vlpt]input 상태값 관리하기

--- src/App.js
+++ src/App.js
@@ -1,61 +1,11 @@
-import React, { Component, Fragment } from 'react';
-import './App.css'
-import MyName from './MyName';
-import MyName1 from './MyName1';
-import Counter from './Counter'
-class App extends Component {
-  render() {
-    const name = "react";
-    const value = 1;
-    return (
-        <Fragment>
-          <div>
-            Hello {name}
-          </div>
-          <div>
-            Bye
-          </div>
-          <div>
-            {
-              1 + 1 === 3
-              ? (<div>맞아요</div>)
-              : (<div>틀려요</div>)
-            }
-          </div>
-          <div>
-            {
-              1 + 1 === 2 && (<div>맞아요!</div>)
-            }
-          </div>
-          <div>
-            {
-              (function(){
-                if(value === 1) return (<div>하나</div>);
-                if(value === 2) return (<div>둘</div>);
-                if(value === 3) return (<div>셋</div>);
-              })()
-            }
-          </div>
-          <div>
-            {
-              (() => {
-                if(value === 1) return (<div>하나</div>);
-                if(value === 2) return (<div>둘</div>);
-                if(value === 3) return (<div>셋</div>);
-              })()
-            }
-          </div>
-          <div className='App'>
-            리액트
-          </div>
-          <MyName name="리액트"></MyName>
-          <MyName />
-          <MyName1 name1="리액트"></MyName1>
-          {/* 주석 */}
-          <Counter/>
-        </Fragment>
-    );
-  }
-}
+import React from 'react';
+import InputSample from './InputSample';
+const App = () => {
+  return (
+    <div>
+      <InputSample/>
+    </div>
+  );
+};
 
 export default App;
