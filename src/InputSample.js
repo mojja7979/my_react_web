027875db7199f706486cf3a0bf0a319a97005841
mojja7979 <pcs386x@naver.com>
2022-01-26 00:38:14 +0900

    [vlpt]여러개의 input 상태값 관리하기

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,21 +1,40 @@
 import React, { useState } from "react";
 
 function InputSample() {
-  const [text, setText] = useState('');
+  console.log(1);
+  const [inputs, setInputs] = useState({
+    name: '',
+    nickname: ''
+  });
+  console.log(2);
+  console.log(inputs);
+  const {name, nickname} = inputs;  //비구조화 할당을 통해 값 추출, 객체의 값을 각각 변수에 할당
+  console.log(name);
+  console.log(nickname);
+
 
   const onChange = (e) => {
-    setText(e.target.value);
+    const {value, name} = e.target; //우선 e.target에서 name과 value를 추출
+    setInputs({
+      ...inputs,  //기존 inputs값을 복사한 뒤
+      [name]: value //name 키를 가진 값을 value로 설정
+    })
   }
 
   const onReset = () => {
-    setText('');
+    setInputs({
+      name: '',
+      nickname: ''
+    })
   }
   return(
     <div>
-      <input onChange={onChange} value={text}/>
+      <input name="name" placeholder="이름" value={name} onChange={onChange}/>
+      <input name="nickname" placeholder="닉네임" value={nickname} onChange={onChange}/>
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값 : {text}</b>
+        <b>값 : </b>
+        {name}({nickname})
       </div>
     </div>  
   );
